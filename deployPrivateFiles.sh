2b80f16d58c577723a0e47d5807fdfbf622dea49
PhantHive <zakaria.chaouki@ipsa.fr>
2023-08-24 23:25:02 +0200

    [deploy]: update copy for .env

--- deployPrivateFiles.sh
+++ deployPrivateFiles.sh
@@ -4,5 +4,8 @@
 SOURCE_DIR="/home/Phearion/aniaPrivate"
 DEST_DIR="$(pwd)/src/assets"
 
+SOURCE_BOT_ENV="/home/Phearion/bots/A-Nia/app/.env"
+
 # Copy files and directories recursively
 cp -a "$SOURCE_DIR"/* "$DEST_DIR"
+cp -r "$SOURCE_BOT_ENV" "$(pwd)}"
